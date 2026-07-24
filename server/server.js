6c3950d9f1eefec53f85d92401e98d6aaf75ed06
Pragashree19 <praga7542@gmail.com>
2026-07-25 01:43:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,70 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Pending requests
+let requests = [];
+
+/* -----------------------------
+   Patient sends a request
+----------------------------- */
+
+app.post("/request", (req, res) => {
+
+    const data = req.body;
+
+    data.id = Date.now();
+
+    data.time = new Date().toLocaleTimeString();
+
+    requests.push(data);
+
+    console.log("New Request:", data);
+
+    res.json({
+        status: "success"
+    });
+
+});
+
+
+/* -----------------------------
+   Dashboard fetches requests
+----------------------------- */
+
+app.get("/requests", (req, res) => {
+
+    res.json(requests);
+
+});
+
+
+/* -----------------------------
+   Nurse acknowledges request
+----------------------------- */
+
+app.delete("/request/:id", (req, res) => {
+
+    const id = Number(req.params.id);
+
+    requests = requests.filter(r => r.id !== id);
+
+    res.json({
+        status: "removed"
+    });
+
+});
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+
+    console.log("TouchVox Server Running");
+    console.log("http://localhost:3000");
+
+});
